f333fd085f4244d30984de904ef1957707be8b73
Kethankrk <kethankrk@gmail.com>
2023-06-09 19:45:29 +0530

    some testing going on here

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,9 +1,99 @@
+const PDFImage = require('pdf-image').PDFImage;
+const {v4: uuidv4} = require("uuid")
+const sharp = require("sharp")
 
-const fistEndpoint = (req, res) => {
-    return res.send("hello world");
+
+const convertPDFToImages = async() => {
+    const id = uuidv4()
+    const pdfImage = new PDFImage('./input.pdf', {
+        convertOptions: {
+            "-quality": "100",
+            "-resize": "800x800",
+        },
+        outputDirectory: "./images",
+        convertExtension: "jpg",
+        pdfFileBaseName: `${uuidv4()}.jpeg`
+    })
+    console.log("converting")
+    const path = await pdfImage.convertFile()
+    console.log("converted")
 }
+
+
+
+const fistEndpoint = async(req, res) => {
+    console.log("Request recived")
+    const date = new Date()
+    await convertPDFToImages();
+    const last = new Date()
+    return res.send(`hello world ${last - date}`);
+};
+
+const convetToPng = async(req, res) => {
+    try{
+        const uid = uuidv4()
+        await sharp("./text.jpg").toFormat('png').toFile(`./images/${uid}.png`)
+        return res.send("Success")
+    }
+    catch(error){
+        return res.send("error")
+    }
+}
+
+
+
+
+// Route for processing the images
+const listConvertion = async (req, res) => {
+  try {
+    const images = req.files;
+
+    // Process each image and convert to PNG
+    const uid = uuidv4()
+    const convertedImages = await Promise.all(
+      images.map(async (image, i) => {
+        const { buffer, originalname } = image;
+        const convertedImage = await sharp(buffer)
+          .toFormat('png')
+          .toFile(`./images/${uid}${i}.png`)
+
+        return {
+          originalname,
+          buffer: convertedImage,
+        };
+      })
+    );
+
+    res.json({ convertedImages });
+  } catch (error) {
+    console.error('Error converting images:', error);
+    res.status(500).json({ error: 'Image conversion failed' });
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = {
-    fistEndpoint
-}
+    fistEndpoint,
+    convetToPng,
+    listConvertion
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const controllers = require("../controller/controller")
+const multer = require('multer');
+
+const upload = multer();
 
 router.get("/img", controllers.fistEndpoint)
+router.get("/topng", controllers.convetToPng)
+router.post("/list", upload.array('files'), controllers.listConvertion)
 
 
 module.exports = router;
